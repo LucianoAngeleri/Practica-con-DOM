15764eb1c8564f7e1a5969bf14f9507dbc1ef268
Luciano Angeleri <lcnang@gmail.com>
2023-04-09 01:31:26 -0300

    Agregamos un nuevo parrafo desde el DOM

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+let botonVerMas = document.getElementById("btnVerMas")
+//Añadir un manejador de eventos
+botonVerMas.addEventListener("click",verMas)
+//Si tengo que enviar argumentos a la función uso una funcion anónioma
+//botonVerMas.addEventListener("click",()=>{ verMas(param1,param2)})
+
+
 function cambiarTitulo(){
     console.log('Desde cambiar Titulo');
     // traer el h1 del html
@@ -11,4 +18,21 @@ function cambiarTitulo(){
     titulo.className = 'text-primary';
     titulo.innerHTML = 'Nuevo titulo desde <b>JS</b>';
     // titulo.innerText = 'Nuevo titulo desde JS';
+}
+function verMas(){
+    console.log("Desde la función 'Ver Mas'")
+    //Traemos el "section" padre
+    let seccionPadre = document.querySelector("#contenedorPadre")
+    //Creamos el nuevo elemento
+    let parrafo = document.createElement("p")
+    parrafo.innerHTML = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, ipsa quam aut odit maxime, eligendi dolor
+    nesciunt pariatur nisi beatae dolore, culpa iste? Quod minus, tenetur incidunt ducimus molestias neque vitae
+    maiores ratione ea perferendis facere nulla, itaque deserunt, totam quas officia. Voluptate cupiditate
+    eveniet, est numquam beatae neque, veritatis debitis ea iusto magni, sapiente totam architecto assumenda.
+    Quasi tempore tenetur`
+    parrafo.className = "lead"
+    //Insertar el nodo hijo
+    //seccionPadre.appendChild(parrafo);
+    //seccionPadre.prepend(parrafo);
+    seccionPadre.insertBefore(parrafo,botonVerMas);
 }
